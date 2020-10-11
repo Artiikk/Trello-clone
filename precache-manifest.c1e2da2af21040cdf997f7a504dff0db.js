2bc1bc71bb88450c72a98918bc7ab7c61eaaaf2b
Artiikk <harfild1@ukr.net>
2020-10-11 14:11:37 +0200

    Updates

---
+++ precache-manifest.c1e2da2af21040cdf997f7a504dff0db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c52acb6ed268afa9f87ef3aac9d434b3",
+    "url": "/Trello-clone/homepage/index.html"
+  },
+  {
+    "revision": "a107bb6f9807d8ff48d3",
+    "url": "/Trello-clone/homepage/static/css/main.8d619ba6.chunk.css"
+  },
+  {
+    "revision": "3ca896d860d969002570",
+    "url": "/Trello-clone/homepage/static/js/2.395b216e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Trello-clone/homepage/static/js/2.395b216e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a107bb6f9807d8ff48d3",
+    "url": "/Trello-clone/homepage/static/js/main.f6cb9ca1.chunk.js"
+  },
+  {
+    "revision": "e2bad803bab8bd771c45",
+    "url": "/Trello-clone/homepage/static/js/runtime-main.972e013b.js"
+  },
+  {
+    "revision": "7b8fab2c7f5fee21346d5f95d1f3f950",
+    "url": "/Trello-clone/homepage/static/media/night_sky.7b8fab2c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Trello-clone/precache-manifest.b90f681dbed4ae57be0fc0e56b001c56.js"
+  "/Trello-clone/homepage/precache-manifest.c1e2da2af21040cdf997f7a504dff0db.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Trello-clone/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Trello-clone/homepage/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
