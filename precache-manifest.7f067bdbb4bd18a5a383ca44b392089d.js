3406d7ee56d78b988f40cc8085960a9039c71257
Artiikk <harfild1@ukr.net>
2020-10-11 13:20:09 +0200

    Updates

---
+++ precache-manifest.7f067bdbb4bd18a5a383ca44b392089d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ed90db7ac5837f22f810cdf240c82a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d676befd22336633c61",
+    "url": "/static/css/main.1ef8672d.chunk.css"
+  },
+  {
+    "revision": "3ca896d860d969002570",
+    "url": "/static/js/2.395b216e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.395b216e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d676befd22336633c61",
+    "url": "/static/js/main.05b4b887.chunk.js"
+  },
+  {
+    "revision": "ec06aec070f49cdd23c4",
+    "url": "/static/js/runtime-main.a29677e0.js"
+  },
+  {
+    "revision": "7b8fab2c7f5fee21346d5f95d1f3f950",
+    "url": "/static/media/night_sky.7b8fab2c.png"
+  }
+]);
